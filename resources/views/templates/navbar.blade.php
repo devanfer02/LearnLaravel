7480ad3c67567365c9bed4f83a69d1da5ee75d07
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-05-07 15:56:14 +0700

    finish part 4

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="position-relative">
     <nav class="navbar navbar-expand-lg navbar-dark bg-danger modified-nav-font">
         <div class="container-fluid" id="nav">
-            <a class="navbar-brand" href="">PHP BLOG</a>
+            <a class="navbar-brand" href="/">PHP BLOG</a>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link custom-nav {{ ($title==="Home") ? 'active' : '' }}" href="/">Home</a>
